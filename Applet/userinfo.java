0b5d162e6287b8d6e0eb6e6ac5caf34a58f86885
Binayak Koirala <108163048+BinayakKoirala@users.noreply.github.com>
2022-07-16 13:52:05 +0545

    Add files via upload

---
+++ Applet/userinfo.java
@@ -0,0 +1,16 @@
+import java.applet.*;
+import java.awt.Graphics;
+
+public class q1 extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("Binayak Koirala", 150, 150);
+        g.drawString("20051954", 150, 200);
+        g.drawString("CSE- 10 ", 150, 250);
+    }
+
+}
+/*
+ * <applet code="q1.class" height=300 width=200>
+ * </applet>
+ */
